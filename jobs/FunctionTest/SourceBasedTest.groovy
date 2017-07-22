06917e7f75e4f17c5af9a3a295676a5a401bce1c
Peter Pan <Peter.Pan@emc.com>
2017-07-22 03:05:36 -0400

    groovy test

--- jobs/FunctionTest/SourceBasedTest.groovy
+++ jobs/FunctionTest/SourceBasedTest.groovy
@@ -1,123 +1,134 @@
-import groovy.transform.Field;
 
-@Field def TEST_TYPE = "manifest"
+class FunctionTestBase implements Serializable {
+    def runtime  //to become a point to Script class instance . lower case 
 
-def generateTestBranches(function_test){
-    def test_branches = [:]
-    node{
-        deleteDir()
-        checkout scm
-        def shareMethod = load("jobs/ShareMethod.groovy")
-        def ALL_TESTS = function_test.getAllTests()
-        def used_resources = function_test.getUsedResources()
-        def TESTS = "${env.TESTS}"
-        if(TESTS == "null" || TESTS == "" || TESTS == null){
-            print "no test need to run"
-            return 
-            
-        }
-
-        def test_stack = "-stack docker_local_run"
-        List tests_group = Arrays.asList(TESTS.split(','))
-        for(int i=0; i<tests_group.size(); i++){
-            def test_name = tests_group[i]
-            def label_name=ALL_TESTS[test_name]["label"]
-            def test_group = ALL_TESTS[test_name]["TEST_GROUP"]
-            def run_fit_test = ALL_TESTS[test_name]["RUN_FIT_TEST"]
-            def run_cit_test = ALL_TESTS[test_name]["RUN_CIT_TEST"]
-            def extra_hw = ALL_TESTS[test_name]["EXTRA_HW"]
-            test_branches["manifest $test_name"] = {
-                String node_name = ""
-                try{
-                    lock(label:label_name,quantity:1){
-                        // Occupy an avaliable resource which contains the label
-                        node_name = shareMethod.occupyAvailableLockedResource(label_name, used_resources)
-                        node(node_name){
-                            withEnv([
-                                "SKIP_PREP_DEP=false",
-                                "USE_VCOMPUTE=${env.USE_VCOMPUTE}",
-                                "HTTP_STATIC_FILES=${env.HTTP_STATIC_FILES}",
-                                "TFTP_STATIC_FILES=${env.TFTP_STATIC_FILES}",
-                                "stash_manifest_name=${env.stash_manifest_name}",
-                                "stash_manifest_path=${env.stash_manifest_path}",
-                                "TEST_TYPE=${TEST_TYPE}"])
-                            {
-                                withCredentials([
-                                    usernamePassword(credentialsId: 'ESXI_CREDS',
-                                                     passwordVariable: 'ESXI_PASS',
-                                                     usernameVariable: 'ESXI_USER'),
-                                    usernamePassword(credentialsId: 'ff7ab8d2-e678-41ef-a46b-dd0e780030e1',
-                                                         passwordVariable: 'SUDO_PASSWORD',
-                                                         usernameVariable: 'SUDO_USER'),
-                                    string(credentialsId: 'INTERNAL_HTTP_ZIP_FILE_URL', variable: 'INTERNAL_HTTP_ZIP_FILE_URL'),
-                                    string(credentialsId: 'INTERNAL_TFTP_ZIP_FILE_URL', variable: 'INTERNAL_TFTP_ZIP_FILE_URL')])
-                                {
-                                deleteDir()
-                                dir("build-config"){
-                                    checkout scm
-                                }
-                                env.BUILD_CONFIG_DIR = "build-config"
-                                // Get the manifest file
-                                unstash "$stash_manifest_name"
-                                env.MANIFEST_FILE="$stash_manifest_path"
-    
-                                // If the manifest file contains PR of on-http and RackHD,
-                                // set the environment variable MODIFY_API_PACKAGE as true
-                                // The test.sh script will install api package according to API_PACKAGE_LIST
-                                sh '''#!/bin/bash
-                                ./build-config/build-release-tools/HWIMO-BUILD ./build-config/build-release-tools/application/parse_manifest.py \
-                                --manifest-file $MANIFEST_FILE \
-                                --parameters-file downstream_file
-                                '''
-                                env.MODIFY_API_PACKAGE = false
-                                if(fileExists ('downstream_file')) {
-                                    def props = readProperties file: 'downstream_file'
-                                    if(props['REPOS_UNDER_TEST']) {
-                                        env.REPOS_UNDER_TEST = "${props.REPOS_UNDER_TEST}"
-                                        def repos = env.REPOS_UNDER_TEST.tokenize(',')
-                                        if(repos.contains("on-http") && repos.contains("RackHD")){
-                                            env.MODIFY_API_PACKAGE = true
-                                        }
-                                    }
-                                }
-                                sh './build-config/jobs/FunctionTest/prepare_common.sh'
-                                retry(3){
-                                    // This scipts can be separated into manifest_src_prepare and common_prepare
-                                    sh './build-config/jobs/FunctionTest/prepare_manifest.sh'
-                                }
-                                step ([$class: 'CopyArtifact',
-                                projectName: 'Docker_Image_Build',
-                                target: "$WORKSPACE"]);
-    
-                                function_test.functionTest(test_name, TEST_TYPE, test_group, run_fit_test, run_cit_test, test_stack, extra_hw)
-                                }
-                            }
-                        }
-                    }
-                } finally{
-                    used_resources.remove(node_name)
-                }
+    def runTests( function_test){
+        def test_branches = generate_test_branch(function_test)
+        runtime.echo "end of generate_test_branch"
+        if(test_branches.size() > 0){
+            try{
+                runtime.parallel test_branches
+            } finally{
+                archiveArtifacts(function_test)
             }
         }
-    }
-    return test_branches
-}
 
-def runTests(function_test){
-    def test_branches = generateTestBranches(function_test)
-    if(test_branches.size() > 0){
-        try{
-            parallel test_branches
-        } finally{
-            archiveArtifacts(function_test)
+    }
+
+    def archiveArtifacts ( f_test)
+    {
+        runtime.echo "archiveArtifacts........."
+    }
+
+    def generate_test_branch(   function_test ){
+        def test_branches = [:]
+        runtime.node{
+            runtime.deleteDir()
+            runtime.checkout runtime.scm
+            def shareMethod = runtime.load("jobs/ShareMethod.groovy") 
+            runtime.echo "Load shareMethod Done."
+            def ALL_TESTS = function_test.getAllTests()//xxxxxxxxxxxxxxxxxxxxxx
+            def TESTS = "${runtime.env.TESTS}"
+            if(TESTS == "null" || TESTS == "" || TESTS == null){
+                print "no test need to run"
+                return 
+                
+            }
+
+            def test_stack = my_test_stack()
+            List tests_group = Arrays.asList(TESTS.split(','))
+            for(int i=0; i<tests_group.size(); i++){
+                def test_name = tests_group[i]
+                def label_name=ALL_TESTS[test_name]["label"]
+                def test_group = ALL_TESTS[test_name]["TEST_GROUP"]
+                def run_fit_test = ALL_TESTS[test_name]["RUN_FIT_TEST"]
+                def extra_hw = ALL_TESTS[test_name]["EXTRA_HW"]
+                runtime.echo "test_branches[$test_name] added"
+                test_branches["$test_name"] = testBranch.curry( function_test , shareMethod,   test_group , run_fit_test, extra_hw, label_name, 15+i ) // create a new Closure by curry
+            }
+
+
         }
+        return test_branches
+    }
+
+    Closure testBranch={ function_test, shareMethod,  test_group , run_fit_test, extra_hw, label_name, my_test_id  ->
+        runtime.echo "testBranch  $test_group , label_name = $label_name "
+        String node_name=""
+        // def used_resources = function_test.getUsedResources()//xxxxxxxxxxxxxxxx
+        //runtime.echo "used_resources = $used_resources"
+        try{
+           runtime.lock(label:label_name,quantity:1){
+                // Occupy an avaliable resource which contains the label
+                // FIXME: I think the shareMethod should be implemented with Closure and delegaion
+                //node_name = shareMethod.occupyAvailableLockedResource(label_name, used_resources)
+                runtime.echo "Get into Try{}"
+
+                node_name="vmslave$my_test_id"
+                runtime.echo "running on $node_name"
+
+                runtime.node(node_name){
+                    runtime.deleteDir()// FIXME, it stucks here. ????????????????????????????????????????????????????????
+                    runtime.dir("build-config"){
+                         checkout runtime.scm
+                    }
+                    
+                    deployRackHD()
+                    runTest()
+                }
+           }
+        } finally{
+            //used_resources.remove(node_name)
+        }
+ 
     }
 }
 
-def archiveArtifacts(function_test){
-    def TESTS = "${env.TESTS}"
-    function_test.archiveArtifactsToTarget("FunctionTest", TESTS, TEST_TYPE)
+class sourceBaseTest extends FunctionTestBase{
+    def TEST_TYPE = "manifest"
+    def my_test_stack = {"-stack docker_local_run"}
+
+    def deployRackHD = {
+        runtime.withEnv([
+            "SKIP_PREP_DEP=false",
+            "XXX=XXXXXXX"])
+        {
+            runtime.withCredentials([
+               runtime.usernamePassword(credentialsId: 'ff7ab8d2-e678-41ef-a46b-dd0e780030e1',
+                                     passwordVariable: 'SUDO_PASSWORD',
+                                     usernameVariable: 'SUDO_USER')]
+            )
+            {
+               runtime.sh '''
+               set -x
+               echo "$SKIP_PREP_DEP"
+               echo "$XXX"
+               '''  
+            }
+        }
+
+
+    }
+    def runTest = {
+         runtime.sh '''
+         echo "I'm sourceBaseTest::runTest()"
+         '''
+    }
+    
+
 }
 
-return this
+
+
+node{
+    deleteDir()
+    checkout scm
+    echo "111111"
+    a=new sourceBaseTest(runtime:this)
+    echo "22222"
+    
+    def function_test = load("jobs/FunctionTest/FunctionTest.groovy")
+    a.runTests( function_test )
+
+}
 
